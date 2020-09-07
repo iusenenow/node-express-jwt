2ef938d01e84e0c9371e6a20413a204b74ecba0d
Wei Zhang <iusenenow@gmail.com>
2020-09-07 13:39:40 +1000

    hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,24 @@
 const User = require('../models/User')
 
+const handleErrors = err => {
+  let errors = { email: '', password: '' }
+
+  // duplicate error code
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered'
+    return errors
+  }
+
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message
+    })
+  }
+
+  return errors
+}
+
 module.exports.signup_get = (req, res) => {
   res.render('signup')
 }
@@ -14,9 +33,9 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password })
     res.status(201).json(user)
-  } catch (error) {
-    console.log(error)
-    res.status(400).send('error, user not created')
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
   }
 }
 
