9026ca2b271e768b8cfcf70a01d74392c2a8197d
Wei Zhang <iusenenow@gmail.com>
2020-09-09 13:15:53 +1000

    finish login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,16 @@ const jwt = require('jsonwebtoken')
 const handleErrors = err => {
   let errors = { email: '', password: '' }
 
+  // incorrect email
+  if (err.message === 'incorrect email') {
+    errors.email = 'that email is not registered'
+  }
+
+  // incorrect password
+  if (err.message === 'incorrect password') {
+    errors.password = 'that password is incorrect'
+  }
+
   // duplicate error code
   if (err.code === 11000) {
     errors.email = 'that email is already registered'
@@ -51,5 +61,16 @@ module.exports.signup_post = async (req, res) => {
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body
+
+  try {
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+    res.status(200).json({ user: user._id })
+  } catch (err) {
+    const errors = handleErrors(err)
+    res.status(400).json({ errors })
+  }
+
   res.send('user login')
 }
